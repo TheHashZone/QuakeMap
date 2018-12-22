1388379f429b722022c51bbd271a521694408404
hash <m.maydew@protonmail.com>
2018-12-23 00:57:53 +0100

    Added .html file and more comments

--- earthquake.js
+++ earthquake.js
@@ -10,20 +10,25 @@ var zoom = 1;
 var earthquakes;
 
 function preload() {
+  // Load the map image with the variables above.
   mapimg = loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/' +
     clon + ',' + clat + ',' + zoom + '/' +
     ww + 'x' + hh +
     '?access_token=pk.eyJ1IjoiaDRzaCIsImEiOiJjamZxN2VtZDkwMTY2MzRwZGxoZGg2MHpoIn0.GDmSi2qY1eJ0GCk4oaCsrA');
+
+    // Load the earthquake data.
   earthquakes = loadStrings('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.csv');
 }
 
-function mercX(lon) {
+// Function for mapping longitude on the map from coordinates.
+function mercX(lon) { 
   lon = radians(lon);
   var a = (256 / PI) * pow(2, zoom);
   var b = lon + PI;
   return a * b;
 }
 
+// Function for mapping latitude on the map from coordinates.
 function mercY(lat) {
   lat = radians(lat);
   var a = (256 / PI) * pow(2, zoom);
@@ -34,33 +39,41 @@ function mercY(lat) {
 
 
 function setup() {
+  // Set up the background to show the image.
   createCanvas(ww, hh);
   translate(width / 2, height / 2);
   imageMode(CENTER);
   image(mapimg, 0, 0);
+
 
   var cx = mercX(clon);
   var cy = mercY(clat);
 
   for (var i = 1; i < earthquakes.length; i++) {
     var data = earthquakes[i].split(/,/);
-    //console.log(data);
+    console.log(data);
+
+    // Clasify latitude, longitude, and magnitude.
     var lat = data[1];
     var lon = data[2];
     var mag = data[4];
     var x = mercX(lon) - cx;
     var y = mercY(lat) - cy;
 
-    if(x < - width/2) {
+    if(x < - width / 2) {
       x += width;
     } 
     else if(x > width / 2) {
       x -= width;
     }
+
+
+    // Drawing the pink dots and changine size depending on magnitude.
     mag = pow(10, mag);
     mag = sqrt(mag);
     var magmax = sqrt(pow(10, 10));
     var d = map(mag, 0, magmax, 0, 180);
+
     stroke(255, 0, 255);
     fill(255, 0, 255, 200);
     ellipse(x, y, d, d);
